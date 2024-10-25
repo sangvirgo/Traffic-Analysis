dbcaa654925bef3fa416a3654c30008f0cb5742a
tansang06092004@gmail.com <tansang06092004@gmail.com>
2024-10-25 21:13:05 +0700

    hihi

--- Version Local/main.py
+++ Version Local/main.py
@@ -51,7 +51,7 @@ class MainApp(QtWidgets.QMainWindow):
 
     def update_frame(self):
         # Đọc một frame từ video
-        ret, img = self.cap.read()
+        ret, frame = self.cap.read()
 
         # Kiểm tra nếu video đã kết thúc
         if not ret:
@@ -60,29 +60,11 @@ class MainApp(QtWidgets.QMainWindow):
             self.timer.stop()
             return
 
-        # Dự đoán các đối tượng trong frame
-        results = self.model.predict(img)
-
-        # Vẽ các bounding box và nhãn cho các đối tượng được phát hiện
-        for r in results:
-            annotator = Annotator(img)
-            boxes = r.boxes
-
-            for box in boxes:
-                # Lấy tọa độ bounding box và nhãn lớp
-                b = box.xyxy[0]  # Tọa độ của bounding box (left, top, right, bottom)
-                c = box.cls  # Chỉ số lớp của đối tượng
-                label = self.model.names[int(c)]  # Lấy tên lớp
-
-                # Kiểm tra nếu đối tượng là phương tiện giao thông
-                if label in self.traffic_labels:
-                    annotator.box_label(b, label)
-
-            # Vẽ kết quả lên frame
-            img = annotator.result()
+        # Gọi hàm xuatloi để xử lý frame và nhận về frame đã xử lý
+        processed_frame = xuatloi(frame)
 
         # Chuyển đổi frame thành định dạng QImage để hiển thị trên QLabel
-        img_resized = cv2.resize(img, (791, 571))  # Kích thước phù hợp với QLabel
+        img_resized = cv2.resize(processed_frame, (791, 571))  # Kích thước phù hợp với QLabel
         img_rgb = cv2.cvtColor(img_resized, cv2.COLOR_BGR2RGB)
         h, w, ch = img_rgb.shape
         bytes_per_line = ch * w
