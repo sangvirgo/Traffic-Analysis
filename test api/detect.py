5b8d454f6b3a4af758873831bc9e215550550b8b
SonVuNgoc <sonvtthanthanh@gmail.com>
2024-10-25 13:24:38 +0700

    huhu

---
+++ test api/detect.py
@@ -0,0 +1,144 @@
+from PIL import ImageFont, ImageDraw, Image
+import numpy as np
+from easyocr import Reader
+import cv2
+import re
+import torch
+import pathlib
+"""
+0: xe may
+1: o to
+2: xe buyt
+3: xe tai
+4: doi mu bao hiem
+5: khong doi mu bao hiem
+6: den do
+7: vach ke duong(vach dung)
+8: boc dau
+9: bien so xe
+
+"""
+pathlib.PosixPath = pathlib.WindowsPath
+model = torch.hub.load('ultralytics/yolov5', 'custom', path='C:\\Traffic-Analysis\\best.pt', force_reload=True)
+
+
+
+""" luu lai bien so vi pham trong file vipham.txt """
+def save_vipham(bienso, kieuvp):
+    with open('vipham.txt', 'a') as logf:
+        logf.write(f'Bien so:{bienso}, vi pham: {kieuvp}\n')
+
+"""Định dạng biển số"""
+def formattedBienSo(bienso):
+    return bienso.strip()  # Trả về biển số đã nhận diện
+
+""" doc bien so tu frame """
+def docBienSo(frame):
+    img = cv2.resize(frame, (800, 600))
+    grayscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)   #chuyen sang mau xam cho toi uu
+    blurred = cv2.GaussianBlur(grayscale, (5, 5), 0)
+    edged = cv2.Canny(blurred, 10, 200)
+
+    contours, _ = cv2.findContours(edged, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours = sorted(contours, key=cv2.contourArea, reverse=True)[:5]
+
+    number_plate_shape = None
+    for c in contours:
+        perimeter = cv2.arcLength(c, True)
+        approximation = cv2.approxPolyDP(c, 0.02 * perimeter, True)
+        if len(approximation) == 4:  # rectangle
+            number_plate_shape = approximation
+            break
+
+    if number_plate_shape is not None:
+        (x, y, w, h) = cv2.boundingRect(number_plate_shape)
+        number_plate = grayscale[y:y + h, x:x + w]
+
+        reader = Reader(['en'])
+        detection = reader.readtext(number_plate)
+
+        if len(detection) == 0:
+            return None  # Không tìm thấy biển số
+        else:
+            return formattedBienSo(detection[0][1])  # Trả về biển số đã nhận diện
+    return None
+
+""" vuot den do """
+def check_vuotdendo(frame, rs):
+    dendo = False
+    vachke = None
+    vehicles = []
+    for det in rs.xyxy[0]:
+        if int(det[-1]) == 6:
+            dendo = True
+        if int(det[-1]) == 7:
+            x1, y1, x2, y2 = map(int, det[:4])
+            vachke = (y1 + y2) // 2 #vi hinh anh theo chieu ngang(nen lay y thay vi x)
+        if int(det[-1]) in [0, 1, 2, 3]:
+            x1, y1, x2, y2 = map(int, det[:4])
+            vehicles.append((y1 + y2) // 2)  #toa do trung tam cua cac xe
+
+    if dendo and vachke is not None:
+        for v in vehicles:
+            if v >= vachke:    #camera thuong o dang sau, nen >=
+                return True
+    return False
+
+""" ko doi mu bao hiem """
+def check_mubh(frame, rs):
+    for det in rs.xyxy[0]:
+        if int(det[-1]) == 5:
+            return True  
+    return False
+
+""" boc dau"""
+def bocdau(frame, rs):
+    for det in rs.xyxy[0]:
+        if int(det[-1]) == 8:
+            return True
+    return False
+""" xuat ra loi vi pham """
+def xuatloi(vid_path):
+    cap = cv2.VideoCapture(vid_path)
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+        results = model(frame)
+        viphams = []
+
+        if check_vuotdendo(frame, results):
+            bienso = docBienSo(frame)
+            if bienso is not None:
+                viphams.append((bienso, "vuot den do"))
+            
+        if check_mubh(frame, results):
+            bienso = docBienSo(frame)
+            if bienso is not None:
+                viphams.append((bienso, "khong doi mu bao hiem"))
+
+        if bocdau(frame, results):
+            bienso = docBienSo(frame)
+            if bienso is not None:
+                viphams.append((bienso, "boc dau"))
+
+        for det in results.xyxy[0]:
+            clsid = int(det[-1])
+            if clsid in [0, 1, 2, 3]:
+                x1, y1, x2, y2 = map(int, det[:4])#in ra cai loi vi pham tren bouding box(mid x vaf tren cung cua y)
+                midx = (x1 + x2) // 2
+                topy = y1
+
+                for bienso, kieuvp in viphams:
+                    save_vipham(bienso, kieuvp)
+                    text = f'{bienso} - {kieuvp}'
+                    cv2.putText(frame, text, (midx, topy), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+                    break
+        cv2.imshow('Bat loi vi pham', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+    
+print(model.names)      #kiem tra xem model co hoat dong
--- test api/main_1.py
+++ test api/main_1.py
@@ -1,8 +1,5 @@
 import requests
-import cv2
-import numpy as np
-from ultralytics import YOLO
-from ultralytics.utils.plotting import Annotator
+from detect import xuatloi
 
 def download_video(url, local_filename):
     # Gửi request để tải video
@@ -18,69 +15,8 @@ def download_video(url, local_filename):
         print("Failed to download video")
         return None
 
-def play_video(video_path):
-    model = YOLO('yolov8n.pt')
 
-    # Khởi tạo capture video từ file video
-    cap = cv2.VideoCapture(video_path)  # Thay bằng đường dẫn tới video của bạn
-
-    # Thiết lập kích thước khung hình
-    cap.set(3, 640)
-    cap.set(4, 480)
-
-    # Danh sách các nhãn phương tiện giao thông cần quan tâm (có thể tùy chỉnh)
-    traffic_labels = ['car', 'motorcycle', 'bus', 'truck', 'bicycle']
-
-    while True:
-        # Đọc một frame từ video
-        ret, img = cap.read()
-
-        # Kiểm tra nếu video đã kết thúc
-        if not ret:
-            print("Kết thúc video")
-            break
-
-        # Dự đoán các đối tượng trong frame
-        results = model.predict(img)
-
-        # Vẽ các bounding box và nhãn cho các đối tượng được phát hiện
-        for r in results:
-            annotator = Annotator(img)
-            boxes = r.boxes
-
-            for box in boxes:
-                # Lấy tọa độ bounding box và nhãn lớp
-                b = box.xyxy[0]  # Tọa độ của bounding box (left, top, right, bottom)
-                c = box.cls  # Chỉ số lớp của đối tượng
-                label = model.names[int(c)]  # Lấy tên lớp
-
-                # Kiểm tra nếu đối tượng là phương tiện giao thông
-                if label in traffic_labels:
-                    annotator.box_label(b, label)
-
-            # Vẽ kết quả lên frame
-            img = annotator.result()
-
-        # Thay đổi kích thước frame và hiển thị
-        img_resized = cv2.resize(img, (960, 540))
-        cv2.imshow('YOLO V8 Traffic Detection', img_resized)
-
-        # Thoát khi nhấn phím ' '
-        if cv2.waitKey(1) & 0xFF == ord(' '):
-            break
-    
-    # Giải phóng các tài nguyên
-    cap.release()
-    cv2.destroyAllWindows()
-
-
-
-
-
-
-
-
-if __name__ == "__main__":
+def main():
     # URL video từ API
     video_url = 'http://127.0.0.1:5000/video/Le Van Viet.mp4'
     
@@ -89,7 +25,9 @@ if __name__ == "__main__":
 
     # Tải video
     downloaded_video = download_video(video_url, local_video_path)
-
-    # Nếu video được tải thành công, tiến hành phát
     if downloaded_video:
-        play_video(downloaded_video)
+        # Xử lý video
+        xuatloi(downloaded_video)
+
+if __name__ == '__main__':
+    main()
